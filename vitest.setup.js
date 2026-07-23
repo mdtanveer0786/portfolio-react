e30f2cff4443db86cc9d1b1905e7d16e628e308a
mdtanveer0786 <mdtanweeralam0002@gmail.com>
2026-07-23 12:10:10 +0530

    test: mock requestAnimationFrame to fix unhandled rejection in vitest

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+// Mock requestAnimationFrame for react-type-animation
+window.requestAnimationFrame = window.requestAnimationFrame || function(callback) {
+    return setTimeout(callback, 0);
+};
+window.cancelAnimationFrame = window.cancelAnimationFrame || function(id) {
+    clearTimeout(id);
+};
